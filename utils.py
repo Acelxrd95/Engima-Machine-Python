d3edbce950d5a107d245add217c238d175901d57
Ace <acelxrd@disroot.org>
2021-10-23 12:58:40 +0200

    Added 2 new utility functions

--- utils.py
+++ utils.py
@@ -1,5 +1,9 @@
+from datastructs.map_struct import Map
+import re
+
+
 def roman2den(roman: str) -> int:
-    r_dict = {"M": 1000, "D": 500, "C": 100, "L": 50, "X": 10, "V": 5, "I": 1}
+    r_dict = Map({"M": 1000, "D": 500, "C": 100, "L": 50, "X": 10, "V": 5, "I": 1})
 
     last, total = 0, 0
     for c in list(roman)[::-1]:
@@ -39,3 +43,47 @@ def n2c(num: int, upper: bool = True) -> str:
     else:
         char = chr(num + 97)
     return char
+
+
+def sp2norm(char: str) -> str:
+    sp_dict = {
+        ".": "YY",
+        ",": "ZZ",
+        "(": "KK",
+        ")": "KK",
+        "{": "KK",
+        "}": "KK",
+        "[": "KK",
+        "]": "KK",
+        "?": "UD",
+        '"': "XX",
+        "'": "XX",
+        "/": "XYX",
+        "\\": "YXY",
+    }
+    char = sp_dict[char]
+    return char
+
+
+def num2word(number: str) -> str:
+    nm_dict = {
+        # "0": ("NULL", "ZERO"),
+        "0": "NULL",
+        "1": "ONE",
+        "2": "TWO",
+        "3": "THREE",
+        "4": "FOUR",
+        "5": "FIVE",
+        "6": "SIX",
+        "7": "SEVEN",
+        "8": "EIGHT",
+        "9": "NINE",
+    }
+    retNum = ""
+    for num in number:
+        retNum += nm_dict[num]
+        retNum += "X"
+    return f"JJ{retNum}JJ"
+
+
+print(num2word("10234"))
