def2c0f892e7f4092c49396650a21fafc8bc4362
Ace <acelxrd@disroot.org>
2021-10-25 11:32:37 +0200

    testing github actions

--- test_enigma.py
+++ test_enigma.py
@@ -29,7 +29,7 @@ class EnsureCipher(unittest.TestCase):
 
     def test_cipher(self):
         enigma = pycipher.Enigma(
-            settings=("A", "A", "A"),
+            settings=("A", "B", "A"),
             rotors=(1, 2, 3),
             reflector="B",
             ringstellung=("A", "A", "A"),
