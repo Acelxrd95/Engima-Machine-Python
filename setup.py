d894a2bfe4194ae3b2b3611903dbc10a2fb626af
Ace <acelxrd@disroot.org>
2021-11-13 15:17:56 +0200

    updated setup.py

--- setup.py
+++ setup.py
@@ -8,8 +8,12 @@ setup(
     author="Yehia Mustafa Gouda",
     author_email="yehia.gouda@tkh.edu.eg",
     packages=find_packages(),
-    entry_points={
-        "console_scripts":["enigma=Enigma.cli:main"]
-    },
+    entry_points={"console_scripts": ["enigma=Enigma.cli:main"]},
+    install_requires=[
+        "click==8.0.3",
+        "Faker>=9.8.0",
+        "pycipher>=0.5.2",
+        "regex==2021.11.2",
+    ],
     test_suite="tests",
 )
